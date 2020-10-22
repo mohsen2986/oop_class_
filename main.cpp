98626483c910abc89c523765c3243a6a0c3a95a1
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-10-22 09:56:51 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Test{
+    private:
+    int a;
+    public:
+    Test(){
+        cout<<"called!!";
+    }
+    Test(int a , int b){
+        this->a = a;
+        this->b = b;
+    }
+    void print(){
+        cout<<this->a<<endl;
+    }
+};
+
+int main() {
+    Test test(10 , 20);
+    test.print();
+    getchar();
+    return 0;
+}
